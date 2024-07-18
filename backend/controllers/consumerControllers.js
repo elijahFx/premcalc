2496a8d2de70a7ef49ce9571df61eba63b775142
elijahFx <killrodxgames@gmail.com>
2024-07-18 16:42:55 +0300

    added the proxy

--- backend/controllers/consumerControllers.js
+++ backend/controllers/consumerControllers.js
@@ -4,7 +4,7 @@ const mongoose = require("mongoose")
 const { JSDOM } = require('jsdom');
 const { HttpsProxyAgent } = require('https-proxy-agent');
 
-const proxyUrl = 'http://178.124.81.210:3128'; // Replace with your proxy URL
+const proxyUrl = 'https://93.85.103.195:8080'; // Replace with your proxy URL
 const agent = new HttpsProxyAgent(proxyUrl);
 
 const url = 'https://service.court.gov.by/ru/public/schedule/schedule';
